1e79936c16580422c402d5b41b1f9b4901752aad
posiek07 <daniel.smolinski@hotmail.com>
2020-08-27 17:04:54 +0100

    Updates

---
+++ precache-manifest.f87982921daa4e02a1f210286ee06f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f49795d01ae6c10f2fe6b788aa08952",
+    "url": "/context-todo/index.html"
+  },
+  {
+    "revision": "598b774868c98571faed",
+    "url": "/context-todo/static/css/2.22863822.chunk.css"
+  },
+  {
+    "revision": "36cfdb34cf3934ad1fb9",
+    "url": "/context-todo/static/css/main.e952ad8b.chunk.css"
+  },
+  {
+    "revision": "598b774868c98571faed",
+    "url": "/context-todo/static/js/2.cd5dda48.chunk.js"
+  },
+  {
+    "revision": "842101171d47fda994e16c0644ee2941",
+    "url": "/context-todo/static/js/2.cd5dda48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36cfdb34cf3934ad1fb9",
+    "url": "/context-todo/static/js/main.be090922.chunk.js"
+  },
+  {
+    "revision": "70cc10ef4c93072b0553",
+    "url": "/context-todo/static/js/runtime-main.f59a0efb.js"
+  }
+]);
